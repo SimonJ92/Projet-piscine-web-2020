89b72e72b1cca89334a8e185631bca3d0466b2b9
Aurèle Clément Duparc <44504667+Elerua@users.noreply.github.com>
2020-04-15 20:56:49 +0200

    Add files via upload

--- Code/Scripts/script_pageclient.js
+++ Code/Scripts/script_pageclient.js
@@ -1,9 +1,44 @@
-function changement_prenom(){
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+			function save(){
+					alert("Sauvegarde effectué");
+					modal.style.display = "none";
+			 }
+			// Get the modal
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+			function changement_prenom(){
 				var prenom = prompt("Entrez votre prénom","Saisir votre Prénom");
 				if(prenom == null || prenom =="" || prenom =="Saisir votre Prénom") {
 					alert("Vous n'avez pas changer de prénom.");
 				}else {
 					document.getElementById("old_prenom").innerHTML = prenom;
+					document.getElementById("prenom").value = prenom;
 					alert("Votre changement a été effectué, voici votre nouveau prénom : "+prenom);
 				}
 			 }
@@ -13,6 +48,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de Nom.");
 				}else {
 					document.getElementById("old_nom").innerHTML = nom;
+					document.getElementById("nom").value = nom;
 					alert("Votre changement a été effectué, voici votre nouveau nom : "+nom);
 				}
 			 }
@@ -22,6 +58,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de mail.");
 				}else {
 					document.getElementById("old_mail").innerHTML = mail;
+					document.getElementById("mail").value = mail;
 					alert("Votre changement a été effectué, voici votre nouveau mail : "+mail);
 				}
 			 }
@@ -31,6 +68,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer d'adresse.");
 				}else {
 					document.getElementById("old_adresse1").innerHTML = adresse1;
+					document.getElementById("adresse1").value = adresse1;
 					alert("Votre changement a été effectué, voici votre nouvelle adresse : "+adresse1);
 				}
 			 }
@@ -40,6 +78,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer d'adresse.");
 				}else {
 					document.getElementById("old_adresse2").innerHTML = adresse2;
+					document.getElementById("adresse2").value = adresse2;
 					alert("Votre changement a été effectué, voici votre nouvelle adresse : "+adresse2);
 				}
 			 }
@@ -49,6 +88,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de ville.");
 				}else {
 					document.getElementById("old_ville").innerHTML = ville;
+					document.getElementById("ville").value = ville;
 					alert("Votre changement a été effectué, voici votre nouvelle ville : "+ville);
 				}
 			 }
@@ -58,6 +98,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de code postal.");
 				}else {
 					document.getElementById("old_zip").innerHTML = zip;
+					document.getElementById("zip").value = zip;
 					alert("Votre changement a été effectué, voici votre nouveau code postal : "+zip);
 				}
 			 }
@@ -67,6 +108,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de pays.");
 				}else {
 					document.getElementById("old_pays").innerHTML = pays;
+					document.getElementById("pays").value = pays;
 					alert("Votre changement a été effectué, voici votre nouveau pays : "+pays);
 				}
 			 }
@@ -76,6 +118,7 @@ function changement_prenom(){
 					alert("Vous n'avez pas changer de numéro de téléphone.");
 				}else {
 					document.getElementById("old_phone").innerHTML = phone;
+					document.getElementById("phone").value = phone;
 					alert("Votre changement a été effectué, voici votre nouveau phone : "+phone);
 				}
 			 }
